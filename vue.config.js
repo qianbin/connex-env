e38bac9e0d4cda5e613669ad743f2071af6a1be8
qianbin <cola.tin.com@gmail.com>
2019-02-01 23:39:34 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
-const PurgecssPlugin = require('purgecss-webpack-plugin')
-const glob = require('glob-all')
-const path = require('path')
-
 module.exports = {
     lintOnSave: false,
     publicPath: process.env.NODE_ENV === 'production' ? '/connex-env/' : '/',
@@ -10,16 +6,5 @@ module.exports = {
             skipWaiting: true,
             clientsClaim: true
         }
-    },
-    configureWebpack: {
-        plugins: [
-            new PurgecssPlugin({
-                paths: glob.sync([
-                    path.join(__dirname, './../src/index.html'),
-                    path.join(__dirname, './../**/*.vue'),
-                    path.join(__dirname, './../src/**/*.js')
-                ])
-            })
-        ]
     }
 }
