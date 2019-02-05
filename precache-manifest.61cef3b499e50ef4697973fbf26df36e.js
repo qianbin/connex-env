b5f8b509361b33860577c7519f7d4769541cd591
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-05 16:21:51 +0000

    Deploy qianbin/connex-env to github.com/qianbin/connex-env.git:gh-pages

---
+++ precache-manifest.61cef3b499e50ef4697973fbf26df36e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0657e9f5a0946248ad77",
+    "url": "/connex-env/css/app.25b74dcb.css"
+  },
+  {
+    "revision": "0657e9f5a0946248ad77",
+    "url": "/connex-env/js/app.2aa38a06.js"
+  },
+  {
+    "revision": "bf4a4038573d58ef25f9",
+    "url": "/connex-env/js/chunk-vendors.9f414872.js"
+  },
+  {
+    "revision": "545ce2e1bc6eb2ea03d9926e927832bc",
+    "url": "/connex-env/img/sync-logo.545ce2e1.png"
+  },
+  {
+    "revision": "2e227108456713d08562f692796470f3",
+    "url": "/connex-env/index.html"
+  },
+  {
+    "revision": "77e982b1ac5a51e0af89b44e12c00ad5",
+    "url": "/connex-env/img/macosx.svg"
+  },
+  {
+    "revision": "62f68f288893b7f58844e03b61f045cf",
+    "url": "/connex-env/img/microsoft-windows.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/connex-env/robots.txt"
+  },
+  {
+    "revision": "2e82d079f8bc66df87636c7bb8f8ab7c",
+    "url": "/connex-env/img/linux-tux.svg"
+  }
+];
